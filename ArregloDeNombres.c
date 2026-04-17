32df4daadc06e0783f0cc64960e4f0b5aeecca06
Franco <francodiezgomez@gmail.com>
2026-04-17 11:25:17 -0300

    buscar nombre 1

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -3,10 +3,12 @@
 #include <string.h>
 
 void MostrarPersonas (char *V[]);
+char * BuscarNombre (int id, char *V[]);
 
 int main(){
-    int i;
-    char *V[4];
+    int i, id;
+    char *V[4], *nombre;
+    nombre=(char *)malloc(50*sizeof(char));
     for (i=0;i<5;i++){
         V[i]=(char *)malloc(50*sizeof(char));
         printf("Ingrese el nombre de la persona %d\n",i+1);
@@ -15,9 +17,15 @@ int main(){
 
     MostrarPersonas(V);
 
+    puts("Ingrese id de la persona para recibir el nombre, del 1 al 5");
+    scanf("%d",&id);
+    nombre= BuscarNombre(id,V);
+    puts(nombre);
+
     for (i=0;i<5;i++){
         free(V[i]);
     }
+    free (nombre);
 
     getchar();
     return 0;
@@ -28,5 +36,19 @@ void MostrarPersonas (char *V[]){
     puts("Lista de personas");
     for (i=0;i<5;i++){
         puts(V[i]);
+    }
+}
+
+char * BuscarNombre (int id, char *V[]){
+    int i;
+    id=id-1;
+    if (id<0 || id>4){
+        return ('\0');
+    } else {
+        for (i=0;i<5;i++){
+            if (id == i){
+                return (V[i]);
+            }
+        }
     }
 }
