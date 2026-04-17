2a11ad9984ea07888c9e7b71a067a65c9edd14f6
Franco <francodiezgomez@gmail.com>
2026-04-17 11:55:23 -0300

    buscar nombre final

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -3,12 +3,13 @@
 #include <string.h>
 
 void MostrarPersonas (char *V[]);
-char * BuscarNombre (char clave[], char *V[]);
+char * BuscaNombrePorId (int id, char *V[]);
+char * BuscaNombrePorPalabra (char clave[], char *V[]);
 
 int main(){
-    int i;
+    int i, id;
     char *V[4],clave[50], *nombre;
-    nombre=(char *) malloc(50 * sizeof(char));
+    nombre=(char *)malloc(50*sizeof(char));
     for (i=0;i<5;i++){
         V[i]=(char *)malloc(50*sizeof(char));
         printf("Ingrese el nombre de la persona %d\n",i+1);
@@ -17,14 +18,21 @@ int main(){
 
     MostrarPersonas(V);
 
+    puts("Ingrese id de la persona para recibir el nombre, del 1 al 5");
+    scanf("%d",&id);
+    nombre= BuscaNombrePorId(id,V);
+    puts(nombre);
+
+    fflush(stdin);
     puts("Ingrese una palabra clave para buscar un nombre en la lista");
     gets(clave);
-    nombre = BuscarNombre(clave,V);
+    nombre = BuscaNombrePorPalabra(clave,V);
     puts(nombre);
 
     for (i=0;i<5;i++){
         free(V[i]);
     }
+    free (nombre);
 
     getchar();
     return 0;
@@ -38,7 +46,21 @@ void MostrarPersonas (char *V[]){
     }
 }
 
-char * BuscarNombre (char clave[], char *V[]){
+char * BuscaNombrePorId (int id, char *V[]){
+    int i;
+    id=id-1;
+    if (id<0 || id>4){
+        return ('\0');
+    } else {
+        for (i=0;i<5;i++){
+            if (id == i){
+                return (V[i]);
+            }
+        }
+    }
+}
+
+char * BuscaNombrePorPalabra(char clave[], char *V[]){
     int i;
     for (i=0;i<5;i++){
         if (strstr(V[i],clave) != NULL){
