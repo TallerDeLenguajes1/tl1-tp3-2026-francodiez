5a3c0f773b3c8e41b8393f3097ee072863505ce1
Franco <francodiezgomez@gmail.com>
2026-04-17 11:35:33 -0300

    branch name 2

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -3,10 +3,12 @@
 #include <string.h>
 
 void MostrarPersonas (char *V[]);
+char * BuscarNombre (char clave[], char *V[]);
 
 int main(){
     int i;
-    char *V[4];
+    char *V[4],clave[50], *nombre;
+    nombre=(char *) malloc(50 * sizeof(char));
     for (i=0;i<5;i++){
         V[i]=(char *)malloc(50*sizeof(char));
         printf("Ingrese el nombre de la persona %d\n",i+1);
@@ -14,6 +16,11 @@ int main(){
     }
 
     MostrarPersonas(V);
+
+    puts("Ingrese una palabra clave para buscar un nombre en la lista");
+    gets(clave);
+    nombre = BuscarNombre(clave,V);
+    puts(nombre);
 
     for (i=0;i<5;i++){
         free(V[i]);
@@ -29,4 +36,14 @@ void MostrarPersonas (char *V[]){
     for (i=0;i<5;i++){
         puts(V[i]);
     }
+}
+
+char * BuscarNombre (char clave[], char *V[]){
+    int i;
+    for (i=0;i<5;i++){
+        if (strstr(V[i],clave) != NULL){
+            return (V[i]);
+        }
+    }
+    return "-1";
 }
